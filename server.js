bbbc54e61c0bc92b9f68b70a6b5ce1c32487d065
ofega <fegaeze@gmail.com>
2019-11-05 07:19:26 +0100

    Written CRUD endpoints

--- server.js
+++ server.js
@@ -6,4 +6,66 @@ const server = express();
 
 server.use(express.json());
 
+  
+// Endpoints
+server.route('/api/accounts')
+    .get((req, res, next) => {
+        const { limit, sortby, sortdir } = req.query;
+        db("accounts").orderBy(sortby || 'id', sortdir || 'asc').limit(limit || 10000)
+            .then(result => {
+                res.status(200).json(result);
+            })
+            .catch((error) => {
+                next(error)
+            })
+    })
+    .post((req, res, next) => {
+        const { name, budget } = req.body;
+        db('accounts').insert({ name, budget })
+            .then(result => {
+                res.status(200).json(result);
+            })
+            .catch((error) => {
+                next(error)
+            })
+    })
+
+
+server.route('/api/accounts/:id')
+    .get((req, res, next) => {
+        const { id } = req.params;
+        db('accounts').where({ id })
+            .then(result => {
+                res.status(200).json(result);
+            })
+            .catch((error) => {
+                next(error)
+            })
+    })
+    .put((req, res, next) => {
+        const { id } = req.params;
+        db('accounts').where({ id }).update({ name, budget })
+            .then(result => {
+                res.status(200).json(result);
+            })
+            .catch((error) => {
+                next(error)
+            })
+    })
+    .delete((req, res, next) => {
+        const { id } = req.params;
+        db('accounts').where({ id }).del()
+            .then(result => {
+                res.status(200).json(result);
+            })
+            .catch((error) => {
+                next(error)
+            })
+    })
+
+
+server.use((error, req, res) => {
+    res.status(500).json({message: error.message})
+})
+
 module.exports = server;
